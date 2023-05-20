de0040dd9cf44d76cd2d9de94e86e2cadbf8b4cc
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2023-05-20 01:10:34 -0400

    Enable search when user clicks search button

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -1,6 +1,9 @@
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import styles from "./searchbar.module.css";
 
 const SearchBar = ({ handleSearch }) => {
+  // If user clicks "Enter" on keyboard
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       const ticker = event.target.value;
@@ -8,16 +11,33 @@ const SearchBar = ({ handleSearch }) => {
     }
   };
 
+  //If user clicks "Search" button on page
+  const handleSearchButton = () => {
+    const searchInput = document.getElementById("searchInput");
+    const ticker = searchInput.value;
+    handleSearch(ticker);
+  };
+
   return (
-    <>
+    <div className={styles.searchContainer}>
       <TextField
-        id="outlined-basic"
+        id="searchInput"
         onKeyDown={handleKeyDown}
         label="Ticker"
         variant="outlined"
         sx={{ width: 300, marginTop: "10px" }}
       />
-    </>
+      <div className={styles.searchButton}>
+        <Button
+          id="searchButton"
+          variant="contained"
+          sx={{ width: 100 }}
+          onClick={handleSearchButton}
+        >
+          Search
+        </Button>
+      </div>
+    </div>
   );
 };
 
